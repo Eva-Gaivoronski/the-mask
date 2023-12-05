c2d37bf365ddd55ceeb16139d25e1c3cceec6af7
Mario Dickerson <dai.dickerson@gmail.com>
2023-12-04 19:57:34 -0600

    Added Base for project

---
+++ src/main/java/com/maskSchedule/maskSchedule/MaskScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.maskSchedule.maskSchedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaskScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaskScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maskSchedule/maskSchedule/MaskScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maskSchedule.maskSchedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaskScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
