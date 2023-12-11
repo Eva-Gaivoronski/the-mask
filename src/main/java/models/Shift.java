1aac20463e78d08fcc7c05750d70c93fb59298ef
Anthony Silva <antsilva93@gmail.com>
2023-12-11 16:30:56 -0600

    worked on some shift stuff, still behind

--- src/main/java/models/Shift.java
+++ src/main/java/models/Shift.java
@@ -1,17 +1,65 @@
 package models;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
+@Entity
 public class Shift {
 
     @Id
     @GeneratedValue
     private int id;
     private Role role;
-    private User user;
+    private Account account;
+    private Employee employee;
     private String ShiftDay;
 
+    //Constructors
 
+    public Shift () {}
 
+    public Shift(Role role, Account account, Employee employee, String ShiftDay) {
+
+    }
+
+    //Hash mapping - need help
+
+    //Getters and Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public String getShiftDay() {
+        return ShiftDay;
+    }
+
+    public void setShiftDay(String shiftDay) {
+        ShiftDay = shiftDay;
+    }
 }
