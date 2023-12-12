8247f8a128c35bcec0ed626cfff9ffe2437a4fd2
Mario Dickerson <dai.dickerson@gmail.com>
2023-12-11 20:30:36 -0600

    added fragments.html

--- src/main/java/com/maskSchedule/maskSchedule/data/ShiftRepository.java
+++ src/main/java/com/maskSchedule/maskSchedule/data/ShiftRepository.java
@@ -1,5 +1,6 @@
 package com.maskSchedule.maskSchedule.data;
 
+import com.maskSchedule.maskSchedule.models.Shift;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
