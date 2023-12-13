7253bb6de2b1b4a800b20bf2c592ce85bd09b2b6
bestCode21 <kingethiopia28@gmail.com>
2023-12-13 00:25:14 -0500

    update User class and add UserRepository.

--- src/main/java/com/maskSchedule/maskSchedule/models/Users.java
+++ src/main/java/com/maskSchedule/maskSchedule/models/Users.java
@@ -2,22 +2,20 @@ package com.maskSchedule.maskSchedule.models;
 
 import jakarta.persistence.Entity;
 import jakarta.validation.constraints.NotNull;
-import org.apache.catalina.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Entity
-public class user {
-
+public class Users {
 
     @NotNull
     private String username;
 
     @NotNull
     private String pwHash;
-    public user(){
+    public Users(){
         // Default constructor
     }
-    public user(String username, String password) {
+    public Users(String username, String password) {
         this.username = username;
         this.pwHash = encoder.encode(password);
     }
@@ -26,10 +24,9 @@ public class user {
         return username;
     }
 
-    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
     public boolean ismatchingPassword(String password){
         return encoder.matches(username,pwHash);
     }
-    // Getters and setters
 }
