f0a22fdaf3c0c10c1921d107765767ffd5d7dc96
bestCode21 <kingethiopia28@gmail.com>
2023-12-13 00:35:07 -0500

    fix ShiftRepository error

--- src/main/java/com/maskSchedule/maskSchedule/data/ShiftRepository.java
+++ src/main/java/com/maskSchedule/maskSchedule/data/ShiftRepository.java
@@ -2,7 +2,9 @@ package com.maskSchedule.maskSchedule.data;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import com.maskSchedule.maskSchedule.models.Shift;
 
 @Repository
 public interface ShiftRepository extends CrudRepository<Shift, Integer> {
+
 }
