57cdb1b35d7e519ed886a64547db0e29f33e1830
daixintheazure <139138591+daixintheazure@users.noreply.github.com>
2023-12-14 20:24:37 -0600

    Revert "Login signup solomon"

--- src/main/java/com/maskSchedule/maskSchedule/data/ShiftRepository.java
+++ src/main/java/com/maskSchedule/maskSchedule/data/ShiftRepository.java
@@ -3,9 +3,7 @@ package com.maskSchedule.maskSchedule.data;
 import com.maskSchedule.maskSchedule.models.Shift;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import com.maskSchedule.maskSchedule.models.Shift;
 
 @Repository
 public interface ShiftRepository extends CrudRepository<Shift, Integer> {
-
 }
